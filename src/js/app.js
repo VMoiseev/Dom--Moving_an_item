ebaa136f9b922917d133f2799396afb5a1613806
VMoiseev <e175@inbox.ru>
2021-12-13 10:52:14 +0500

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,30 @@
+const boardSize = 4;
+const playingField = document.getElementById('field');
+
+if (playingField) {
+  for (let i = 0; i < boardSize ** 2; i += 1) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    playingField.appendChild(cell);
+  }
+}
+
+const cells = Array.from(document.querySelectorAll('.cell'));
+
+function goblinImg(index, number) {
+  if (index !== -1) {
+    cells[index].classList.remove('goblin');
+  }
+  const goblinImgCell = cells[number];
+  goblinImgCell.classList.add('goblin');
+}
+
+setInterval(() => {
+  const index = cells.findIndex((item) => item.className.includes('goblin'));
+  let number = Math.floor(Math.random() * cells.length);
+  if (number === index) {
+    number = Math.floor(Math.random() * cells.length);
+  } else {
+    goblinImg(index, number);
+  }
+}, 1000);
